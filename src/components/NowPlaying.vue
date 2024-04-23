97baf8d170919ffd2191db266acdd4ff83c6c5b9
Peet0103 <96548482+Peet0103@users.noreply.github.com>
2024-04-23 15:35:33 +0200

    leuke tekst toegevoegd

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -18,7 +18,7 @@
       </div>
     </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
-      <h1 class="now-playing__idle-heading">No music is playing 😔</h1>
+      <h1 class="now-playing__idle-heading">het is stil aan de overkant O_O</h1>
     </div>
   </div>
 </template>
